ca770bf78e8c9acc90302f1933da0cb4b90976d0
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2022-12-05 12:09:05 +0100

    Other schemas for the TreeLDR generated context.

--- scripts/tldr-to-context.sh
+++ scripts/tldr-to-context.sh
@@ -12,10 +12,25 @@ tldrc \
 	-i spec/tldr/extern/odrl.tldr \
 	-i spec/tldr/extern/credentials.tldr \
 	-i spec/tldr/rebase.tldr \
+	-i spec/tldr/common/dns.tldr \
+	-i spec/tldr/common/email.tldr \
 	-i spec/tldr/common/github.tldr \
+	-i spec/tldr/common/reddit.tldr \
+	-i spec/tldr/common/soundcloud.tldr \
+	-i spec/tldr/common/twitter.tldr \
 	json-ld-context --rdf-type-to-layout-name \
 	-m https://www.w3.org/=spec/extern/www.w3.org/ \
 	-c https://www.w3.org/2018/credentials/v1.jsonld \
+	https://w3id.org/rebase#DnsVerification \
+	https://w3id.org/rebase#DnsVerificationMessage \
+	https://w3id.org/rebase#EmailVerification \
+	https://w3id.org/rebase#EmailVerificationMessage \
 	https://w3id.org/rebase#GitHubVerification \
 	https://w3id.org/rebase#GitHubVerificationMessage \
+	https://w3id.org/rebase#RedditVerification \
+	https://w3id.org/rebase#RedditVerificationMessage \
+	https://w3id.org/rebase#SoundCloudVerification \
+	https://w3id.org/rebase#SoundCloudVerificationMessage \
+	https://w3id.org/rebase#TwitterVerification \
+	https://w3id.org/rebase#TwitterVerificationPublicTweet \
 > $1
